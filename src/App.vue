7e289e2d05f8808d2d20ef17588b3c04987505a9
Rendy Artha <rendyartha312@gmail.com>
2025-07-30 15:53:02 +0700

    feat: handle when user update from the list

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { computed, onMounted, ref, watch } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { computed, onMounted, ref } from "vue";
 import { useStore } from "vuex";
 
 const store = useStore();
 
 const todo = ref("");
+const modeUpdate = ref(false);
+const editingTodo = ref<{ id?: number; title?: string } | null>(null);
 
 const handleAddTodo = () => {
   const payload = {
@@ -13,6 +14,38 @@ const handleAddTodo = () => {
   };
   store.dispatch("todoModule/addTodo", payload);
   todo.value = "";
+};
+
+const handleUpdateTodo = () => {
+  if (!editingTodo.value) return;
+
+  const payloadUpdate = {
+    ...editingTodo.value,
+    title: todo.value,
+  };
+
+  store.dispatch("todoModule/updateTodo", payloadUpdate);
+  modeUpdate.value = false;
+  todo.value = "";
+  editingTodo.value = null;
+};
+
+const handleSubmitTodo = () => {
+  if (modeUpdate) {
+    return handleUpdateTodo();
+  }
+
+  return handleAddTodo();
+};
+
+const handleChangeMode = (todoItem: any) => {
+  modeUpdate.value = true;
+  editingTodo.value = { ...todoItem };
+  todo.value = todoItem.title;
+};
+
+const handleCancelUpdate = () => {
+  modeUpdate.value = false;
 };
 
 const handleDelete = (id: number) => {
@@ -33,13 +66,17 @@ const todoList = computed(() => store.getters["todoModule/allTodo"]);
 <template>
   <div>
     <input v-model="todo" />
-    <button @click="handleAddTodo">Input</button>
+    <button @click="handleSubmitTodo">
+      {{ modeUpdate ? "Update" : "Add" }}
+    </button>
+    <button v-if="modeUpdate" @click="handleCancelUpdate">Cancel Update</button>
     <ul>
       <li v-for="todo in todoList">
         <p>
           {{ todo.title }}
         </p>
         <button @click="handleDelete(todo.id)">Delete</button>
+        <button @click="handleChangeMode(todo)">Edit</button>
       </li>
     </ul>
   </div>
